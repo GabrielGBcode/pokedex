ab135e326038de4fb4b5b3c5bbd59ba8c7a1896c
Gabriel GB <105217214+GabrielGBcode@users.noreply.github.com>
2023-09-01 14:04:00 -0300

    Criando arquivos para desafio extra

---
+++ assets/js/pokemon-stats.js
@@ -0,0 +1,6 @@
+const pokeName = document.querySelector("li.name");
+const pokeID = document.querySelector("li.id");
+const pokeType = document.querySelector("li.type");
+const pokePhoto = document.querySelector("li.photo");
+const pokeStats = document.querySelector("li.stats");
+const url = "https://pokeapi.co/api/v2/pokemon/1";
